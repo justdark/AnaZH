a2e36a15206b98a6c02ac9ae57f74ec7b5d9e10e
yshwaker <yshwaker@gmail.com>
2014-12-02 16:44:39 +0800

    optimize code

--- js/main.js
+++ js/main.js
@@ -1,47 +1,3 @@
-document.getElementByClass = function(n) { 
-            var el = [],
-                _el = document.getElementsByTagName('div');
-            for (var i=0; i<_el.length; i++ ) {
- 
-                if (_el[i].className.substring(0,n.length) == n ) {
-                    el[el.length] = _el[i];
-                }
-            }
-            return el;
-        }
-function getElementByClass(documents,tag,n) { 
-            var el = [],
-                _el = documents.getElementsByTagName(tag);
-            for (var i=0; i<_el.length; i++ ) {
- 
-                if (_el[i].className.substring(0,n.length) == n ) {
-                    el[el.length] = _el[i];
-                }
-            }
-            return el;
-        }
-document.getElementByHref = function(n) { 
-            var el = [],
-                _el = document.getElementsByTagName('div');
-            for (var i=0; i<_el.length; i++ ) {
- 
-                if (_el[i].getAttribute("href").substring(0,n.length) == n ) {
-                    el[el.length] = _el[i];
-                }
-            }
-            return el;
-        }
-function getElementByHref(documents,n) { 
-            var el = [],
-                _el = documents.getElementsByTagName('a');
-            for (var i=0; i<_el.length; i++ ) {
- 
-                if (_el[i].getAttribute("href").substring(0,n.length) == n ) {
-                    el[el.length] = _el[i];
-                }
-            }
-            return el;
-        }
 function ANSWER_INFO()
 {
     this.usercount = 0
@@ -54,113 +10,67 @@ function ANSWER_INFO()
 
 function parseDom(arg) { 
     var xmlDoc = null;
-    xmlDoc = new DOMParser().parseFromString(arg, "text/xml");
-    return xmlDoc
-　　 //var objE = document.createElement("div"); 
-　　 //objE.innerHTML = arg; 
-　　 //return objE.childNodes; 
+    xmlDoc = new DOMParser().parseFromString(arg, "text/html");
+    return xmlDoc;
 };  
- function parseDomNode(arg) { 
 
-　　 var objE = document.createElement("div"); 
-　　 objE.innerHTML = arg; 
-　　 return objE.childNodes; 
-};     
-    function ajaxRequest(url,ids)
+//get all voters info 
+function ajaxRequest(url,ids)
+{
+    var xmlHttpRequest = null;
+    if(window.XMLHttpRequest)
     {
-
-        var xmlHttpRequest = null;
-        if(window.ActiveXObject) // IE浏览器
-        {
-            xmlHttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        else if(window.XMLHttpRequest) // 除IE以外的其他浏览器
-        {
-            xmlHttpRequest = new XMLHttpRequest();
-        }
-
-
-        if(null != xmlHttpRequest)
-        {
-            //GET方式向服务器发出一个请求
-            xmlHttpRequest.open("GET", url, true);
-             
-            // 当发生状态变化时就调用这个回调函数
-            xmlHttpRequest.onreadystatechange = function(){
-            if(xmlHttpRequest.readyState == 4)
-            {
-            //alert("aaaaaa"+ids);
-                //alert(ids);
-
-                if (xmlHttpRequest.status == 200) {
-
-                var content = xmlHttpRequest.responseText;
-                console.log(url)
-                //document.getElementById("newEle"+ids).innerHTML += content;
-                var UserDOM = parseDom(content);
-                var users = getElementByClass(UserDOM,"a","zg-link-gray");
-                //alert("user's length: "+users.length)
-				if (users.length==0)
-				{
-					document.getElementById("newEle"+ids).innerHTML = "No Upper at all";
-				}
-                AnswerList[ids].anonymous = UserDOM.getElementsByTagName("span").length-2- users.length;
-
-                for (var i in users)
-                {
-                    var tmp = users[i].getAttribute("href")
-
-                    var uid = tmp.substring(8,tmp.length);
-                    //alert(uid)
-
-                    ajaxRequestUser(ids,uid);
-                }
-            }
-
-            }}; ;
-            
-            // 向服务器发出一个请求
-            xmlHttpRequest.send(null);    
-        }
-
+        xmlHttpRequest = new XMLHttpRequest();
     }
 
-    function ajaxRequestUser(ids,users)
+    if(null != xmlHttpRequest)
     {
-        
-        var xmlHttpRequest = null;
-        if(window.ActiveXObject) // IE浏览器
-        {
-            xmlHttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        xmlHttpRequest.open("GET", url, true);
+        xmlHttpRequest.onreadystatechange = function(){
+            if (xmlHttpRequest.readyState == 4){
+                if (xmlHttpRequest.status == 200) {
+                    var content = xmlHttpRequest.responseText;
+                    var UserDOM = parseDom(content);
+                    var users = UserDOM.getElementsByTagName("a");
+                    if (users.length==0){
+                        document.getElementById("newEle"+ids).innerHTML = "No Upper at all";
+                    }
+                    AnswerList[ids].anonymous = UserDOM.getElementsByTagName("span").length-2- users.length;
+                    for (var i in users)
+                    {
+                        var tmp = users[i].getAttribute("href");
+                        var uid = tmp.substring(8,tmp.length);
+                        ajaxRequestUser(ids,uid);
+                    }
+                }
+            }
         }
-        else if(window.XMLHttpRequest) // 除IE以外的其他浏览器
-        {
-            xmlHttpRequest = new XMLHttpRequest();
-        }
-        //alert(users)
-        var url = "http://www.zhihu.com/node/MemberProfileCardV2?params=%7B%22url_token%22%3A%22"+ users +"%22%7D"
-        if(null != xmlHttpRequest)
-        {
-            //GET方式向服务器发出一个请求
-            xmlHttpRequest.open("GET", url, true);
-             
-            // 当发生状态变化时就调用这个回调函数
-            xmlHttpRequest.onreadystatechange = function(){
+        xmlHttpRequest.send(null); 
+    }
+}
+
+//get user detail info
+function ajaxRequestUser(ids,users)
+{
+
+    var xmlHttpRequest = null;
+    if(window.XMLHttpRequest)
+    {
+        xmlHttpRequest = new XMLHttpRequest();
+    }
+    var url = "http://www.zhihu.com/node/MemberProfileCardV2?params=%7B%22url_token%22%3A%22"+ users +"%22%7D"
+    if(null != xmlHttpRequest)
+    {
+        xmlHttpRequest.open("GET", url, true);
+        xmlHttpRequest.onreadystatechange = function(){
             if(xmlHttpRequest.readyState == 4)
             {
-            //alert("aaaaaa"+ids);
-                
                 var content = xmlHttpRequest.responseText;
                 var newDOM = parseDom(content);
-                var newNodelist = parseDomNode(content);
-                //alert(newNodelist.item(0).getElementsByTagName("span").length)
-                //alert(new XMLSerializer().serializeToString(newDOM));
-                //alert("s'")
-                var values = getElementByClass(newNodelist.item(0),"span","value")
-                //alert(values[0].textContent)
-                var answers = values[0].textContent;
-                var posts = values[1].textContent;
-                var followers = values[2].textContent;
+                var values = newDOM.getElementsByClassName("value");
+                var answers = values[0].innerHTML;
+                var posts = values[1].innerHTML;
+                var followers = values[2].innerHTML;
                 AnswerList[ids].usercount+=1;
                 AnswerList[ids].answers += parseInt(answers);
                 AnswerList[ids].posts += parseInt(posts);
@@ -170,53 +80,41 @@ function parseDom(arg) {
                     AnswerList[ids].tripleZero+=1;
                 }
 
-                //alert("hahahahaha:::  "+ids+" "+users+" "+answers+" "+posts+" "+followers);
-                document.getElementById("newEle"+ids).innerHTML = "Totally " +(AnswerList[ids].usercount+AnswerList[ids].anonymous)+" Uppers,Include "+AnswerList[ids].tripleZero+" tripleZero User "+"And "+AnswerList[ids].anonymous+" anonymous Uppers"+"<br>"
-                                                                    +"Among all user ,three key values are #Answers:" + AnswerList[ids].answers+"  #Posts:"+AnswerList[ids].posts+
-                                                                    "  #Followers:"+AnswerList[ids].followers+"  "+"<br>";
-            
-            }}; ;
-            
-            // 向服务器发出一个请求
-            xmlHttpRequest.send(null);    
+                document.getElementById("newEle"+ids).innerHTML = "Totally " +(AnswerList[ids].usercount+AnswerList[ids].anonymous)+" voters, Include "+AnswerList[ids].tripleZero+" tripleZero voters "+"And "+AnswerList[ids].anonymous+" anonymous voters"+"<br>"
+                +"Among all voters, aggregate amount is #Answers: " + AnswerList[ids].answers+"  #Posts: "+AnswerList[ids].posts+
+                "  #Followers: "+AnswerList[ids].followers+"  "+"<br>";
+            }
         }
+        xmlHttpRequest.send(null);    
+    }
 
-    }    
-    
-//userInformationUrl = "http://www.zhihu.com/node/MemberProfileCardV2?params=%7B%22url_token%22%3A%22"+userid+"%22%7D"
-var greeting = "hola, ";
+}    
 
-var mores = document.getElementByClass("zm-votebar");
-//alert(mores.length)
+//main
+var mores = document.getElementsByClassName("zm-votebar");
 AnswerList = new Array();
-for (var i in mores)
+for (var i = 0; i < mores.length; ++i)
 {
     var s = document.createElement('div');
-    s.style.borderColor = "#0769CD";
-    s.style.borderStyle = "solid";
-    s.style.borderWidth = "1px";
-    s.style.backgroundColor = "#0769CD";
-    s.style.color = "#FFFFFF" ;
-    s.style.padding = "10px";
-	s.style.cursor = "pointer"
     var ids = mores[i].parentNode.getAttribute('data-aid');
     s.innerHTML = "To be calculated";
+    s.setAttribute("class","stat-addon");
     s.id = "newEle" +  ids;
 
     var askurl = "http://www.zhihu.com/node/AnswerFullVoteInfoV2?params=%7B%22answer_id%22%3A%22"+ ids +"%22%7D";
-	
+
     ANSWER_INFO()
     AnswerList[ids] = new ANSWER_INFO();
-    
-//    s.onClick = new Function("alert('assdds');") function(){ 
-//                            ajaxRequest(askurl, ids);}
+
     mores[i].parentNode.insertBefore(s, mores[i]);
-    //ajaxRequest(askurl, ids);
-    document.getElementById("newEle" +  ids).addEventListener('click',function () {
-                                                                var id = this.getAttribute("id").substring(6,this.getAttribute("id").length);
-                                                                var url = "http://www.zhihu.com/node/AnswerFullVoteInfoV2?params=%7B%22answer_id%22%3A%22"+ id +"%22%7D";
-																if (AnswerList[id].usercount==0)
-																	{ajaxRequest(url,id);}
-																},false); 
+    document.getElementById("newEle" +  ids)
+    .addEventListener('click',function () {
+        var id = this.getAttribute("id").substring(6,this.getAttribute("id").length);
+        var url = "http://www.zhihu.com/node/AnswerFullVoteInfoV2?params=%7B%22answer_id%22%3A%22"+ id +"%22%7D";
+        if (AnswerList[id].usercount==0) {
+            ajaxRequest(url,id);
+            console.log("click");
+        }
+    },false); 
 
 }
